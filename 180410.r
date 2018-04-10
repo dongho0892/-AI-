853e67c0930470c787e3bbfd212684c0e9ff6312
dongho0892 <35053718+dongho0892@users.noreply.github.com>
2018-04-10 13:27:28 +0900

    수업내용

---
+++ 180410.r
@@ -0,0 +1,20 @@
+help(sum)
+?sum
+example(mean)
+??("normal distribution")
+
+1:5
+5:1
+
+x <-  c(1,2,3)
+y <- c(4,5,c(6,7,8))
+
+?seq
+seq (from=1, to=5, by=2)
+seq(1,5,by=2)
+seq(0,1,by=0.001)
+
+?rep
+rep(c(1,2,3),times=2)
+rep(c(1,2,3),each=2)
+
